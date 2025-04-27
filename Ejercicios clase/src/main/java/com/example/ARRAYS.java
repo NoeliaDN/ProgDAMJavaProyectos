3f97fa90eae6c27b57c1d0707fdf333aebbd54fd
Noelia Delgado Noguerales <noelia.delgado@tajamar365.com>
2025-04-27 12:23:26 +0200

    reestructurando carpetas y creando nuevo proyecto para las matrices.

---
+++ Ejercicios clase/src/main/java/com/example/ARRAYS.java
@@ -0,0 +1,25 @@
+package com.example;
+
+import java.util.Scanner;
+
+public class ARRAYS {
+    public static void main(String[] args) {
+
+        int [] notas = new int[3]; // tiene tamaño 3, por lo que ahí estaría el límite de datos a introducir en este ejemplo, siempre empezando en index 0.
+        Scanner entrada = new Scanner(System.in);
+
+        System.out.println("Dime la primera nota: ");
+        notas[0] = entrada.nextInt();
+
+        System.out.println("Dime la segunda nota: ");
+        notas[1] = entrada.nextInt();
+
+        System.out.println("Dime la tercera nota: ");
+        notas[2] = entrada.nextInt();
+
+        System.out.println("\nLa primera nota es: " + notas[0]);
+        System.out.println("\nLa segunda nota es: " + notas[1]);
+        System.out.println("\nLa tercera nota es: " + notas[2]);
+
+    }
+}
